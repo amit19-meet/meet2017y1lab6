619a3f38fb7bbcabcfa4d6b40140ba294999e2f0
amit19-meet <amit19@meet.mit.edu>
2017-07-31 15:38:00 +0300

    Stopping here

---
+++ funturtle.py
@@ -0,0 +1,31 @@
+import turtle #python needs this to use all the turtle functions
+#turtle.shape("turtle") #changes the shape to a turtle
+#square = turtle.clone() #creates new turtle and saves it in square
+#square.shape("square") # changes the shape of the second turtle to a square
+#square.goto(100, 100) # moves square to ( x= 100, y= 100)
+#square.goto(100,0)
+#square.goto(100,100)
+#square.goto(0,100)
+#square.goto(0,0)
+#triangle = turtle.clone() #creates new turtle and saves it in triangle
+#triangle.shape("triangle") #changes the shape of the third turtle to a triangle
+#triangle.goto(100,0)
+#triangle.goto(50,25)
+#triangle.goto(0,0)
+#square.penup()
+#square.goto(200,200)
+#square.stamp()
+#square.goto(100,50)
+#triangle.penup()
+#triangle.goto(200,200)
+#triangle.stamp()
+#triangle.goto(100,50)
+UP = 0
+LEFT = 1
+DOWN = 2
+RIGHT = 3
+direction = UP
+
+
+
+turtle.mainloop() #makes this the last line in ypur turtle code
